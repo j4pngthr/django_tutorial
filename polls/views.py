50c44c3628453f50c699fe7aeffe7cbddab8f47d
j4pngthr <weibeicun@gmail.com>
2022-05-21 00:51:15 +0900

    tutorial page3終わり

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
+# from django.http import Http404
+# from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 # from django.template import loader
@@ -22,11 +23,13 @@ def index(request):
 def detail(request, question_id):
   # return HttpResponse("You're looking at question {}.".format(question_id))
   # return HttpResponse("You're looking at question %s." % question_id)
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
-  return render(request, 'polls/detail.html', {'question': question})
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+  # return render(request, 'polls/detail.html', {'question': question})
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', { 'question':question })
   
 def results(request, question_id):
   response = "You're looking at the results of question %s."
