e2a941e413395c45316ba9ad3fc1b16fcf6d0d51
j4pngthr <weibeicun@gmail.com>
2022-05-21 00:39:40 +0900

    エラーの送出

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 # from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,11 +18,16 @@ def index(request):
   }
   # return HttpResponse(template.render(context, request))
   return render(request, 'polls/index.html', context)
-  
+
 def detail(request, question_id):
   # return HttpResponse("You're looking at question {}.".format(question_id))
-  return HttpResponse("You're looking at question %s." % question_id)
-
+  # return HttpResponse("You're looking at question %s." % question_id)
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', {'question': question})
+  
 def results(request, question_id):
   response = "You're looking at the results of question %s."
   return HttpResponse(response % question_id)
