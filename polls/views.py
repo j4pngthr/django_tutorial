580803f870de8ff2d9d55ac9ed8776827e9aedae
j4pngthr <weibeicun@gmail.com>
2022-05-26 23:10:11 +0900

    ビューの詳細を書く

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
 from django.template import loader
 
-from .models import Question
+from .models import Choice, Question
 
 # URLに対応づけてURLconfに反映させればブラウザで文字を表示
 # def index(request):
@@ -54,8 +54,24 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', { 'question':question })
 
 def results(request, question_id): # 質問結果ページ
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  # response = "You're looking at the results of question %s."
+  # return HttpResponse(response % question_id)
+  # detailとほぼ同じ
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id): # 投票ページ
-  return HttpResponse("You're voting on question %s." % question_id)
+  # ダミー実装
+  # return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist): # 上のtryでPOSTデータにchoiceがない
+    return render(request, 'polls/detail.html', { # エラーメッセージ付きの質問フォーム
+      'question': question,
+      'error_message': "You didn't select a choice",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
