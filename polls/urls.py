58f3e1eb45771fa8b37b86888ece8421c3bac091
j4pngthr <weibeicun@gmail.com>
2022-07-03 22:10:58 +0900

    汎用ビューの作成

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,15 @@ from . import views
 
 app_name = 'polls' # pollsアプリの名前空間をURLconfに追加
 urlpatterns = [ # viewとURLの対応付け
-  path('', views.index, name='index'), # index
-  path('<int:question_id>/', views.detail, name='detail'), # detail
-  path('<int:question_id>/results/', views.results, name='results'), # results
-  path('<int:question_id>/vote/', views.vote, name='vote'), # vote
+  # path('', views.index, name='index'), # index
+  # path('<int:question_id>/', views.detail, name='detail'), # detail
+  # path('<int:question_id>/results/', views.results, name='results'), # results
+  # path('<int:question_id>/vote/', views.vote, name='vote'), # vote
+
   # path('specifics/<int:question_id>/', views.detail, name='detail'),
+
+  path('', views.IndexView.as_view(), name='index'),
+  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+  path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+  path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 from django.template import loader
@@ -48,17 +49,33 @@ def index(request):
 #     raise Http404("Question does not exist")
 #   return render(request, 'polls/detail.html', {'question': question})
 
-# ショートカット Http404
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/detail.html', { 'question':question })
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id): # 質問結果ページ
-  # response = "You're looking at the results of question %s."
-  # return HttpResponse(response % question_id)
-  # detailとほぼ同じ
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+# ショートカット Http404
+# def detail(request, question_id):
+#   question = get_object_or_404(Question, pk=question_id)
+#   return render(request, 'polls/detail.html', { 'question':question })
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def results(request, question_id): # 質問結果ページ
+#   # response = "You're looking at the results of question %s."
+#   # return HttpResponse(response % question_id)
+#   # detailとほぼ同じ
+#   question = get_object_or_404(Question, pk=question_id)
+#   return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id): # 投票ページ
   # ダミー実装
